bd80e43d45c1e15e290a800b26365ba60904f9a2
Ambika Sahu <90752801+AmbikaSahu2577@users.noreply.github.com>
2023-05-20 17:00:08 +0530

    Add files via upload

---
+++ proggram5.java
@@ -0,0 +1,13 @@
+package com.exam;
+
+public class proggram5 {
+
+	public static void main(String[] args) {
+		String str[] = {"abc","pqr","xyz"};
+      for (String string : str) 
+      {
+		  System.out.println(string);
+	  }
+	}
+
+}
